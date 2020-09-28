7a940058b474fa08ffa80633dfc394f8eb67c6f7
Mateus Franklin <contato@mateusfranklin.com>
2020-09-28 16:21:54 -0300

    Acerto na exibicao e ocultacao do botao resultado

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -1,7 +1,7 @@
 //Capturar evento de submit do formulário
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     //escuta evento e impedi de enviar formulario
     const inputPeso = e.target.querySelector('#peso');
@@ -11,18 +11,18 @@ form.addEventListener('submit', function(e){
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
     //tenta converter pra number
-    
+
     //se o peso não for verdadeiro da a mensagem de peso invalido e flag false pra adicionar a classe
     if (!peso) {
-        setResultado ('Peso Invalido', false);
+        setResultado('Peso Invalido', false);
         return;
     }
     //se altura não for verdadeira da a mensagem de altura invalid e flag false pra adicionar a classe
-    if(!altura) {
-        setResultado ('Altura Invalida', false);
+    if (!altura) {
+        setResultado('Altura Invalida', false);
         return;
     }
-    
+
     const imc = getImc(peso, altura);
     const nivelImc = getNivelImc(imc);
 
@@ -31,61 +31,56 @@ form.addEventListener('submit', function(e){
     setResultado(msg, true);
 });
 
-function getNivelImc (imc) {
+function getNivelImc(imc) {
     const nivel = ['Abaixo do Peso', ' Peso Normal', 'Sobrepeso',
-     'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
-     if (imc >= 39.9) return nivel[5];
-     if (imc >= 34.9) return nivel[4];
-     if (imc >= 29.9) return nivel[3];
-     if (imc >= 24.9) return nivel[2];
-     if (imc >= 18.5) return nivel[1];
-     if (imc < 18.5)  return nivel[0];
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 }
 
-function getImc (peso, altura) {
+function getImc(peso, altura) {
     const imc = peso / altura ** 2;
     return imc.toFixed(2);
 }
 
-function criaP () {
+function criaP() {
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado (msg, isValid) {
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
-    // resultado.style.display = 'block';
-    
+    document.getElementById('resultado').style.display = 'block';
+
     resultado.innerHTML = '';
 
-    const p = criaP ();
-    
+    const p = criaP();
+
     if (isValid) {
         p.classList.add('paragrafo-resultado');
-    }else {
+    } else {
         p.classList.add('bad');
     };
 
     p.innerHTML = msg;
     resultado.appendChild(p);
 }
-    
+
 function limparImc() {
-    if(document.getElementById('peso').value!="" || document.getElementById('altura').value!="") {
+    if (document.getElementById('peso').value != "" || document.getElementById('altura').value != "") {
         document.getElementById('altura').value = '';
-        document.getElementById('peso').value= '';
+        document.getElementById('peso').value = '';
         // const div = document.querySelector('#resultado');
         // let div1 = div.style.display = "none";
         // if(div1 == "none") {
         //     div1 = div.style.display = "inline-block";
         // }
-        
+
     }
-        const el = document.getElementById('resultado').style.display;
-        if(el == "none"){
-            document.getElementById(el).style.display = 'block';
-        }else{
-            document.getElementById(el).style.display = 'none';
-        }
+    document.getElementById('resultado').style.display = 'none';
 }
